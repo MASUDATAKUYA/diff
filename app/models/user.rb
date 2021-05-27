e62547161571cac3932cabc6c4ec0a6ff7f89323
Masuda <yotsuba0404@icloud.com>
2021-05-28 08:25:17 +0900

    コミットメッセージ

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  validates :name, {presence: true}
+  validates :email, {presence: true, uniqueness: true}
+
+  def posts
+   return Post.where(user_id: self.id)
+  end
+
+  def imageposts
+    return Post.where(user_id: self.id)
+  end
+
+end
---
+++ db/migrate/20210527193114_add_image_id_to_posts.rb
@@ -0,0 +1,4 @@
+class AddImageIdToPosts < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
